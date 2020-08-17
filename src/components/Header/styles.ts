131559c131a63eb64a921bcbf8b139e90949e52a
Layanne Cantanhede <layrodrgs@gmail.com>
2020-08-17 11:55:55 -0300

    iniciando estilizacao

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,13 @@
 import styled from 'styled-components';
+import { FaGithub } from 'react-icons/fa';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    background: var(--header);
+    padding: 11px 16px;
+`;
+
+export const GithubLogo = styled(FaGithub)``;
+
+export const SearchForm = styled.form``;
